752b64c65c17cae84266f376e53902fb70f60874
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-08-29 21:41:08 +0300

    Task 0 : Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Log parsing """
+import sys
+import re
+
+
+def print_stats(size: int, status: dict) -> None:
+    """ Print stats """
+    print("File size: {}".format(size))
+    for key in sorted(status.keys()):
+        if status[key] > 0:
+            print("{}: {}".format(key, status[key]))
+
+
+if __name__ == "__main__":
+    status = {"200": 0, "301": 0, "400": 0,
+              "401": 0, "403": 0, "404": 0,
+              "405": 0, "500": 0}
+    size = 0
+    count = 0
+    try:
+        for line in sys.stdin:
+            try:
+                line = line.strip()
+                rgex = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - ' + \
+                    r'\[\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\.\d{1,6}\]' +\
+                    r' "GET /projects/260 HTTP/1.1" (\d{1,3}) (\d{1,4})$'
+                if not re.fullmatch(rgex, line):
+                    continue
+
+                count += 1
+                line = line.split()
+
+                if line[-2] in status:
+                    status[line[-2]] += 1
+                    size += int(line[-1])
+
+                if count % 10 == 0:
+                    print_stats(size, status)
+
+            except Exception:
+                if count % 10 == 0:
+                    print_stats(size, status)
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        print_stats(size, status)
